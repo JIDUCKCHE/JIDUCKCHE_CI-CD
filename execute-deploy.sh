ba8c24f383d6d7c8f98fc1e55b39fd088e272b17
rycando <rycando@naver.com>
2021-01-03 22:42:15 +0900

    test ci/cd

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -4,5 +4,5 @@ cd /home/ubuntu/JIDUCKCHE_CI-CD
 cp -rf /home/ubuntu/live.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
 cp -rf /home/ubuntu/dev.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
 
-docker build -t jiduckche:1.001 .
+docker build -t jiduckche:1.0 .
 ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
