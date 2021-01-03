d1f58b54711d85bc43d4bbf6e7656a1df5bfe692
rycando <rycando@naver.com>
2021-01-03 22:13:43 +0900

    test ci/cd

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ DOCKER_APP_NAME=jiduckche
 
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
-if [ -z "$EXIST_BLUE" ]; then
+if [ -z "$EXIST_BLUE" ]; then 
     echo "blue up"
     docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d
     sleep 10
