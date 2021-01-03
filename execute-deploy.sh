611a0f3592d6c627727bad6c0143e18f9879bba1
rycando <rycando@naver.com>
2021-01-03 21:36:21 +0900

    modify excute-deploy.sh

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -3,4 +3,6 @@
 cd /home/ubuntu/JIDUCKCHE_CI-CD
 cp -rf /home/ubuntu/live.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
 cp -rf /home/ubuntu/dev.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
+
+docker build -t jiduckche:1.0 .
 ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
