d9ca23510f2f39a27477b8c50fe64bf772758d12
rycando <rycando@naver.com>
2021-01-03 15:31:50 +0900

    ci/cd fix error

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-cp -rf /home/ubuntu/live.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
-cp -rf /home/ubuntu/dev.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
-
 DOCKER_APP_NAME=jiduckche
 
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/JIDUCKCHE_CI-CD
+cp -rf /home/ubuntu/live.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
+cp -rf /home/ubuntu/dev.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
 ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
