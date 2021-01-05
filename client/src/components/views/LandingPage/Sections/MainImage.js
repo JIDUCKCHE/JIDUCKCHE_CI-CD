c7750722168f619e3ca16871a133ce343d062668
rycando <rycando@naver.com>
2021-01-05 11:56:17 +0900

    add - header info

--- client/src/components/views/LandingPage/Sections/MainImage.js
+++ client/src/components/views/LandingPage/Sections/MainImage.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 function MainImage(props) {
     return (
+        // <div style={{ width: '100%', height: '500px' }}>
+        //     <CarouselProvider
+        //         naturalSlideWidth={10}
+        //         naturalSlideHeight={10}
+        //         totalSlides={3}
+        //         visibleSlides={3}
+        //         infinite={true}
+        //     >
+        //         <Slider>
+        //             <Slide index={0}>First Slide</Slide>
+        //             <Slide index={1}>Second Slide</Slide>
+        //             <Slide index={2}>Third Slide</Slide>
+        //         </Slider>
+        //         <ButtonBack>Back</ButtonBack>
+        //         <ButtonNext>Next</ButtonNext>
+        //     </CarouselProvider>
+        // </div>
         <a href={`/prod/${props.prodId}`}>
             <div style={{ background: `linear-gradient(to bottom, rgba(0,0,0,0)
             39%,rgba(0,0,0,0)
