ef52543c60767dcf0c2dbd22e9082a2fe91e1bc1
rycando <rycando@naver.com>
2021-01-21 00:55:29 +0900

    add naver search advisor html file uploaded

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -5,6 +5,7 @@ cp -rf /home/ubuntu/live.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
 cp -rf /home/ubuntu/dev.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
 cp -rf ./sitemap.xml ./client/build/
 cp -rf ./robots.txt ./client/build/
+cp -rf ./naver47b1da586467d02b4058841075956419.html ./client/build/
 
 
 docker build -t jiduckche:1.0 .
