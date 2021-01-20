c20f7e3d562f598443ebd5e4f612711ae79381e4
rycando <rycando@naver.com>
2021-01-21 00:46:37 +0900

    add robots.txt & sitemap.xml

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -3,6 +3,9 @@
 cd /home/ubuntu/deploy/JIDUCKCHE_CI-CD
 cp -rf /home/ubuntu/live.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
 cp -rf /home/ubuntu/dev.js /home/ubuntu/deploy/JIDUCKCHE_CI-CD/server/config/
+cp -rf ./sitemap.xml ./client/build/
+cp -rf ./robots.txt ./client/build/
+
 
 docker build -t jiduckche:1.0 .
 ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
