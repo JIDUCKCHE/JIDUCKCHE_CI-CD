10af0dd95e4820d7c1ea32d5f3adfe417ddc80cb
rycando <rycando@naver.com>
2021-04-19 21:18:05 +0900

    fix index.js] error while renew ssl certification

--- server/index.js
+++ server/index.js
@@ -1,41 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
 
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser")
-const mongoose = require('mongoose');
-const config = require('./config/key');
-const logger = require('./logger')
-const morgan = require('morgan')
-const cors = require('cors');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const config = require("./config/key");
+const logger = require("./logger");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
 
 const port = config.port;
 
 mongoose
-    .connect(config.mongoURI, config.mongoConfig)
-    .then(() => console.log("MongoDB Connected..."))
-    .catch((err) => console.log(err));
+	.connect(config.mongoURI, config.mongoConfig)
+	.then(() => console.log("MongoDB Connected..."))
+	.catch((err) => console.log(err));
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(morgan('combined', {stream: logger.stream}));
-app.use(express.static('public'));
-app.use(express.static('client/build'));
+app.use(morgan("combined", { stream: logger.stream }));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static("client/build"));
 
-app.use('/api/user', require('./routes/user'));
-app.use('/api/artist', require('./routes/artist'));
-app.use('/api/prod', require('./routes/prod'));
-app.use('/api/notice', require('./routes/notice'));
-app.use('/api/ent', require('./routes/ent'));
-app.use('/api/comment', require('./routes/comment'));
-app.use('/api/like', require('./routes/like'));
-app.use('/api/favorite', require('./routes/favorite'));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/artist", require("./routes/artist"));
+app.use("/api/prod", require("./routes/prod"));
+app.use("/api/notice", require("./routes/notice"));
+app.use("/api/ent", require("./routes/ent"));
+app.use("/api/comment", require("./routes/comment"));
+app.use("/api/like", require("./routes/like"));
+app.use("/api/favorite", require("./routes/favorite"));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))});
+	res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
